7931349ccc35e0fee823ffdd61150c83c33dde21
JAYESH <jayjdahivale@gmail.com>
2022-04-18 11:29:47 +0530

    [Jayesh]:added the test user registration file

--- Programs/test_user_registration.py
+++ Programs/test_user_registration.py
@@ -11,17 +11,30 @@ import sys
 from user_registration import *
 
 
-
 class TestUserRegistration(unittest.TestCase):
 
     def test_f_name_length_error(self):
         with self.assertRaises(LengthError):
             first_name("ja")
+
     def test_f_name_missing(self):
         with self.assertRaises(MissingValueError):
             first_name()
+
     def test_pattern_validity_f_name(self):
-        self.assertEqual(first_name("Jayesh"),True)
-        self.assertEqual(first_name("jayesh"),False)
+        self.assertEqual(first_name("Jayesh"), True)
+        self.assertEqual(first_name("jayesh"), False)
         self.assertEqual(first_name("Jayesh34323"), False)
 
+    def test_l_name_length_error(self):
+        with self.assertRaises(LengthError):
+            last_name("ja")
+
+    def test_l_name_missing(self):
+        with self.assertRaises(MissingValueError):
+            last_name()
+
+    def test_pattern_validity_l_name(self):
+        self.assertEqual(last_name("Jayesh"), True)
+        self.assertEqual(last_name("jayesh"), False)
+        self.assertEqual(last_name("Jayesh34323"), False)
