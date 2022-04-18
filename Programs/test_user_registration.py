65a7025c580d313e0956a7ad17e7b572001a299e
JAYESH <jayjdahivale@gmail.com>
2022-04-18 12:19:54 +0530

    [Jayesh]:modified test_user registration cases

--- Programs/test_user_registration.py
+++ Programs/test_user_registration.py
@@ -1,17 +1,19 @@
-'''
+"""
 @Author: Jayesh Dahiwale
 @Date: 2022-04-18 18:00:30
 @Last Modified by: Jayesh Dahiwale
 @Last Modified time: 2021-04-18 18:00:30
 @Title : User Registration Problems
-'''
+"""
 import unittest
-import logging
-import sys
+
 from user_registration import *
 
 
 class TestUserRegistration(unittest.TestCase):
+    def test_f_name_invalid_argument(self):
+        with self.assertRaises(InvalidArgumentType):
+            first_name(11)
 
     def test_f_name_length_error(self):
         with self.assertRaises(LengthError):
@@ -26,6 +28,10 @@ class TestUserRegistration(unittest.TestCase):
         self.assertEqual(first_name("jayesh"), False)
         self.assertEqual(first_name("Jayesh34323"), False)
 
+    def test_l_name_invalid_argument(self):
+        with self.assertRaises(InvalidArgumentType):
+            last_name(11)
+
     def test_l_name_length_error(self):
         with self.assertRaises(LengthError):
             last_name("ja")
@@ -38,3 +44,18 @@ class TestUserRegistration(unittest.TestCase):
         self.assertEqual(last_name("Jayesh"), True)
         self.assertEqual(last_name("jayesh"), False)
         self.assertEqual(last_name("Jayesh34323"), False)
+
+    def test_email_invalid_argument(self):
+        with self.assertRaises(InvalidArgumentType):
+            valid_email(11)
+
+    def test_email_missing(self):
+        with self.assertRaises(MissingValueError):
+            valid_email()
+
+    def test_pattern_valid_name(self):
+        self.assertEqual(valid_email("jayesh.dahiwale@bl.co"), True)
+
+
+if __name__ == "__main__":
+    TestUserRegistration()
